be434ebbea0bb54cb685620c8549b9642e709104
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-12-12 15:11:14 +0700

    Create DefinitioNotFound.cs

---
+++ Exceptions/DefinitioNotFound.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UrbanDictionary.Exceptions
+{
+	public class DefinitioNotFound : Exception
+	{
+		public DefinitioNotFound(string msg) : base(msg)
+		{
+
+		}
+
+		public DefinitioNotFound(string msg, Exception e) : base(msg, e.InnerException)
+		{
+
+		}
+	}
+}
